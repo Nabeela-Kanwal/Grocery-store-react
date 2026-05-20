5e0229c4abd42435791278f2d440bffa9445cd4e
Nabeela Kanwal <codewithnabeela@gmail.com>
2026-05-20 19:06:33 +0500

    nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <nav className="flex">
+        <a href="">
+          Gr<span>O</span>cery
+        </a>
+        <ul className="flex">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            About <a href="">About Us</a>
+          </li>
+          <li>
+            Process <a href="">Process</a>
+          </li>
+          <li>
+            Contact <a href="">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
