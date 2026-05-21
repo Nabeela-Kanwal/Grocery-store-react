f0f95ef9e2c5b98692aad89e75bbb211df363f2b
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-21 19:39:48 +0500

    updated the clases

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="bg-red-300">
+    <header className="bg-white">
       <nav className="max-w-[1400px] mx-auto px-10 h-[14vh] flex justify-between items-center">
-        <a href="">
-          Gr<span>O</span>cery
+        <a href="#" className="text-3xl font-bold">
+          Gr<span className="text-orange-500">O</span>cery
         </a>
-        <ul className="flex">
+        <ul className="flex items-center gap-x-15">
           <li>
-            <a href="">Home</a> 
+            <a href="" className="font-semibold tracking-wider text-orange-500">Home</a> 
           </li>
           <li>
-             <a href="">About Us</a>
+             <a href="" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500">About Us</a>
           </li>
           <li>
-             <a href="">Process</a>
+             <a href="" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500">Process</a>
           </li>
           <li>
-            <a href="">Contact Us</a>
+            <a href="" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500">Contact Us</a>
           </li>
         </ul>
       </nav>
