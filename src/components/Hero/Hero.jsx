f3d920255c282f2b6ffb884a0bcfb2146b62edd8
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-22 20:42:25 +0500

    updated the code

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,13 @@ const Hero = () => {
     <section>
       <div className="max-w-[1400px] mx-auto px-10">
         <div>
-          <span className="bg-orange-100 text-orange-500 text-lg px-5 py-3 rounded-full">Export Best Quality...</span>
+          <span className="bg-orange-100 text-orange-500 text-lg px-5 py-3 rounded-full">
+            Export Best Quality...
+          </span>
+          <h1 className="text-7xl font-bold">
+            Tasty Organic <span className="text-orange-500">Fruits</span> &{" "}
+            <span className="text-orange-500">Veggies</span> In Your City
+          </h1>
         </div>
         <div>
           <img src={Grocery} alt="" />
