adb162c5c8c4605d4664ae53b1a5e49029dba78b
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-22 20:38:10 +0500

    span class added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,9 @@ const Hero = () => {
   return (
     <section>
       <div className="max-w-[1400px] mx-auto px-10">
-        <div></div>
+        <div>
+          <span className="bg-orange-100 text-orange-500 text-lg px-5 py-3 rounded-full">Export Best Quality...</span>
+        </div>
         <div>
           <img src={Grocery} alt="" />
         </div>
