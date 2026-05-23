c4de31dc389bd4b96757a204772d3a43c1866f83
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-23 21:08:09 +0500

    Enhance Hero and Navbar components: Add minimum height to Hero section, adjust padding, and update Navbar for better responsiveness with menu icon.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import Button from "../Button/Button";
 const Hero = () => {
   return (
     <section>
-      <div className="max-w-[1400px] mx-auto px-10 flex items-center">
+      <div className="min-h-screen max-w-[1400px] mx-auto px-10 flex items-center pt-25">
         <div className="flex-1">
           <span className="bg-orange-100 text-orange-500 text-lg px-5 py-3 rounded-full">
             Export Best Quality...
@@ -13,7 +13,7 @@ const Hero = () => {
             Tasty Organic <span className="text-orange-500">Fruits</span> &{" "}
             <span className="text-orange-500">Veggies</span> <br /> In Your City
           </h1>
-          <p className="text-zinc-600 text-lg max-w-[530px] mt-5 mb-10" >
+          <p className="text-zinc-600 text-lg max-w-[530px] mt-5 mb-10">
             Bred for a high content of beneficial substances. Our products are
             all fresh and healthy.
           </p>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import { GoHeartFill } from "react-icons/go";
 import { HiMiniShoppingBag } from "react-icons/hi2";
 import { IoSearchSharp } from "react-icons/io5";
+import { TbMenu2 } from "react-icons/tb";
 
 const Navbar = () => {
   return (
-    <header className="bg-white">
+    <header className="bg-white fixed top-0 right-0 left-0">
       <nav className="max-w-[1400px] mx-auto px-10 h-[14vh] flex justify-between items-center">
         <a href="#" className="text-3xl font-bold">
           Gr<span className="text-orange-500">O</span>cery
         </a>
-        <ul className="flex items-center gap-x-15">
+        <ul className="md:flex flex items-center gap-x-15 hidden">
           <li>
             <a href="" className="font-semibold tracking-wider text-orange-500">
               Home
@@ -44,7 +45,7 @@ const Navbar = () => {
           </li>
         </ul>
         <div className="flex items-center gap-x-5">
-          <div className="flex items-center p-1 border-2 border-orange-500 rounded-full">
+          <div className="md:flex items-center p-1 border-2 border-orange-500 rounded-full hidden">
             <input
               type="text"
               name="text"
@@ -65,6 +66,11 @@ const Navbar = () => {
           <a href="#" className="text-zinc-800 text-2xl">
             <HiMiniShoppingBag />
           </a>
+
+           <a href="#" className="text-zinc-800 text-3xl md:hidden">
+            < TbMenu2/>
+          </a>
+
         </div>
       </nav>
     </header>
