a2cc8e316a82e1b2001d7ae4c6744be1c194e1e7
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-23 20:57:52 +0500

    fixed the alignment

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,22 +4,23 @@ import Button from "../Button/Button";
 const Hero = () => {
   return (
     <section>
-      <div className="max-w-[1400px] mx-auto px-10">
-        <div>
+      <div className="max-w-[1400px] mx-auto px-10 flex items-center">
+        <div className="flex-1">
           <span className="bg-orange-100 text-orange-500 text-lg px-5 py-3 rounded-full">
             Export Best Quality...
           </span>
-          <h1 className="text-7xl font-bold">
+          <h1 className="text-7xl/20 font-bold mt-4">
             Tasty Organic <span className="text-orange-500">Fruits</span> &{" "}
-            <span className="text-orange-500">Veggies</span> In Your City
+            <span className="text-orange-500">Veggies</span> <br /> In Your City
           </h1>
-          <p className="text-zinc-600 text-lg">
+          <p className="text-zinc-600 text-lg max-w-[530px] mt-5 mb-10" >
             Bred for a high content of beneficial substances. Our products are
             all fresh and healthy.
           </p>
           <Button content="Shop Now" />
         </div>
-        <div>
+
+        <div className="flex-1">
           <img src={Grocery} alt="" />
         </div>
       </div>
