977a5f38a5c68e4da7dd01274db2a4ae9b89ac0f
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-23 21:27:26 +0500

    fixed the sidebar in mobile view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -67,11 +67,58 @@ const Navbar = () => {
             <HiMiniShoppingBag />
           </a>
 
-           <a href="#" className="text-zinc-800 text-3xl md:hidden">
-            < TbMenu2/>
+          <a href="#" className="text-zinc-800 text-3xl md:hidden">
+            <TbMenu2 />
           </a>
-
         </div>
+        {/* mobile menu */}{" "}
+        <ul className="flex flex-col gap-y-15 bg-orange-500/15 backdrop-blur-xl rounded-xl p-10 items-center gap-x-15 md:hidden absolute top-30 -left-full transform -translate-x-1/2">
+          <li>
+            <a href="" className="font-semibold tracking-wider text-orange-500">
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              About Us
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Process
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Contact Us
+            </a>
+          </li>
+          <li className="flex items-center p-1 border-2 border-orange-500 rounded-full md:hidden">
+            <input
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Search..."
+              className="flex-1 h-10 px-3 focus:outline-none"
+            />
+
+            <button className="bg-orange-600 text-white w-10 h-10 p-0 flex justify-center items-center rounded-full text-xl leading-none">
+              <IoSearchSharp className="block" />
+            </button>
+          </li>
+        </ul>
       </nav>
     </header>
   );
