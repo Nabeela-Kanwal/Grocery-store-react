d31a8956607b52542313dacc08ffae63c6050d7c
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-24 20:35:13 +0500

    inluded the category function

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Heading from "../Heading/Heading";
+import fruitsCat from "../../assets/fruits-and-veggies.png";
+import Dairyat from "../../assets/dairy-and-eggs.png";
+import SeaFoodCat from "../../assets/meat-and-seafood.png";
+
 
 const Category = () => {
   return (
@@ -10,3 +14,29 @@ const Category = () => {
 };
 
 export default Category;
+
+const category = [
+  {
+    id: 1,
+    title: "fruits and Veggies",
+    description:
+      "Fresh, organic produce sourced daily from local farms. Explore a wide range of seasonal fruits and crisp vegetables.",
+    image: fruitsCat,
+  },
+
+  {
+    id: 2,
+    title: "Dairy & Eggs",
+    description:
+      "Wholesome dairy products and free-range eggs. From creamy milk and yogurt to artisanal cheeses.",
+    image: Dairyat,
+  },
+
+  {
+    id: 3,
+    title: "Meet & SeaFood",
+    description:
+      "High-quality, responsibly sourced meat and seafood. Choose from fresh cuts, marinated options, and more.",
+    image: SeaFoodCat,
+  },
+];
