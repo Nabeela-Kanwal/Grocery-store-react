9228ff1281321816ac7c2faec966f06ee612fffa
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-26 22:24:04 +0500

    import the vallues component

---
+++ src/components/Values/Values.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "../Heading/Heading";
+
+const Values = () => {
+  return (
+    <section>
+      <div className="max-w-[1400px] macx-auto px-10">
+        <Heading highlight="Our" heading="Values" />
+      </div>
+    </section>
+  );
+};
+
+export default Values;
