455b0543d27aed79dfb50d2d03f2c9ede8f9f716
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-29 21:48:54 +0500

    products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Products = () => {
   return;
   <section>
-    <div></div>
+    <div className="max-w-[1400px] mx-auto px-10 py-20">
+      <Heading highlight="Our" heading="Products" />
+    </div>
   </section>;
 };
 
