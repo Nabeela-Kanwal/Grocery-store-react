d71a66fe6b83ca6bd32ee145e9c052388786aa96
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-29 22:09:19 +0500

    included the useState function

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "../Heading/Heading";
 
 const Products = () => {
-  const categories = ["ALl", "Fruits", "Vegetables", "Dairy", "Sea Food"];
+  const categories = ["All", "Fruits", "Vegetables", "Dairy", "Sea Food"];
+  const [activeTab, setActiveTab] = useState("All");
   return (
     <section>
       <div className="max-w-[1400px] mx-auto px-10 py-20">
         <Heading highlight="Our" heading="Products" />
 
         {/* Tabs */}
-        <div>
+        <div className="flex gap-3 justify-center mt-10">
           {categories.map((category) => {
             return (
-              <button key={category} className="zinc-100 px-5 py-2 text-lg ">
+              <button
+                key={category}
+                className={`px-5 py-2 text-lg rounded-lg
+                  ${activeTab === category ? "bg-green-700 text-white" : "bg-white text-zinc-800"}`}
+                onClick={() => setActiveTab(category)}
+              >
                 {category}
               </button>
             );
