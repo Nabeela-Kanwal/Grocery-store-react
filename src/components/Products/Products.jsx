42d15c06fc1fb4bc97b3625a6647d4f30cf895ed
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-29 21:58:18 +0500

    maped the categories

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import Heading from "../Heading/Heading";
 
 const Products = () => {
+  const categories = ["ALl", "Fruits", "Vegetables", "Dairy", "Sea Food"];
   return (
     <section>
       <div className="max-w-[1400px] mx-auto px-10 py-20">
         <Heading highlight="Our" heading="Products" />
+
+        {/* Tabs */}
+        <div>
+          {categories.map((category) => {
+            return <button>{category}</button>;
+          })}
+        </div>
       </div>
     </section>
   );
