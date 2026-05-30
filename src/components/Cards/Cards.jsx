a68dedfa799868b8b53b40692f02225f39689345
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-30 17:25:14 +0500

    card component

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaHeart, FaPlus } from "react-icons/fa";
+
+const Cards = () => {
+  return (
+    <div>
+      <div>
+        <span>
+          <FaHeart />
+        </span>
+        <button>
+          <FaPlus />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
