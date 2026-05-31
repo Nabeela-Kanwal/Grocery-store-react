d3b71c1d547e5104ad230e037d81dc5308d4a93a
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-05-31 12:44:37 +0500

    fixed issue

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -4,9 +4,9 @@ import Heading from "../Heading/Heading";
 const Process = () => {
   return (
     <section>
-      <div className="max-w-[1400px] max-auto py-10">
+      <div className="max-w-[1400px] mx-auto px-10 py-20">
         <div className="w-fit mr-auto ">
-            <Heading highlight="Our" heading="Process" />
+          <Heading highlight="Our" heading="Process" />
         </div>
       </div>
     </section>
