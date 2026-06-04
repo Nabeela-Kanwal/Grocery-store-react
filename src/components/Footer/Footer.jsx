1759b85c653b74d6d1e4d7c5f46b176c9f35916b
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-04 18:10:29 +0500

    fixed width

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-zinc-100 py-20">
-      <div className="max-w-[1400] mx-auto py-10">
+      <div className="max-w-[1400px] mx-auto py-10">
         <div>
           <a href="#" className="text-3xl font-bold">
             Gr<span className="text-green-800">O</span>cery
