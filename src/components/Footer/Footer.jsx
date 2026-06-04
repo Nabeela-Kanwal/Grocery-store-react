5f3c49e224ef2804b8eee76f577696226239d103
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-04 18:07:37 +0500

    inluded logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="bg-zinc-100 py-20">
-    <div>
-      
-    </div>
-  </footer>;
+  return (
+    <footer className="bg-zinc-100 py-20">
+      <div className="max-w-[1400] mx-auto py-10">
+        <div>
+          <a href="#" className="text-3xl font-bold">
+            Gr<span className="text-green-800">O</span>cery
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
