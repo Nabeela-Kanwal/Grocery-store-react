25d3f1799622ab2a892e493e980f5a065ac07c8c
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-04 18:13:27 +0500

    added text in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,6 +8,10 @@ const Footer = () => {
           <a href="#" className="text-3xl font-bold">
             Gr<span className="text-green-800">O</span>cery
           </a>
+          <p className="text-zinc-600 mt-4">
+            Bred for a high content of beneficial substances. Our products are
+            all fresh and healthy.
+          </p>
         </div>
       </div>
     </footer>
