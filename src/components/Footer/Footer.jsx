d8339ab547691ee458d57f554f2a6fb5a44f8b94
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-04 18:16:49 +0500

    para in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,9 +8,12 @@ const Footer = () => {
           <a href="#" className="text-3xl font-bold">
             Gr<span className="text-green-800">O</span>cery
           </a>
-          <p className="text-zinc-600 mt-4">
+          <p className="text-zinc-600 mt-4 max-w-[350px]">
             Bred for a high content of beneficial substances. Our products are
             all fresh and healthy.
+          </p>
+          <p className="text-zinc-800 mt-6">
+            2025 &copy; Grocery. All rights reserved.
           </p>
         </div>
       </div>
