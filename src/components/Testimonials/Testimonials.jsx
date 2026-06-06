c73655970096b77b41f82ebf52e39f552bb28faa
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-06 22:04:24 +0500

    icon added

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Heading from "../Heading/Heading";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Testimonials = () => {
   return (
     <section>
       <div className="max-w-[1400px] px-10 mx-auto py-20">
         <Heading highlight="Customers" heading="Saying" />
+        <div className="py-5">
+          <button>
+            <IoIosArrowForward />
+          </button>
+        </div>
       </div>
     </section>
   );
