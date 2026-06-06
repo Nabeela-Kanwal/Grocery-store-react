c0c2ff3a90aee057f624533186160c6d5f79edbc
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-06 22:10:50 +0500

    hover effect

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -8,11 +8,11 @@ const Testimonials = () => {
       <div className="max-w-[1400px] px-10 mx-auto py-20">
         <Heading highlight="Customers" heading="Saying" />
         <div className=" flex justify-end py-5 gap-x-3">
-          <button className="text-2xl rounded-lg w-11 h-11 flex justify-center items-center bg-zinc-100">
+          <button className="text-2xl text-zinc-800 rounded-lg w-11 h-11 flex justify-center items-center bg-zinc-100 hover:bg-gradient-to-b hover:from-green-700 hover:to-green-800 text-white cursor-pointer">
             <IoIosArrowBack />
           </button>
 
-          <button className="text-2xl rounded-lg w-11 h-11 flex justify-center items-center bg-zinc-100">
+          <button className="text-2xl text-zinc-800 rounded-lg w-11 h-11 flex justify-center items-center bg-zinc-100 hover:bg-gradient-to-b hover:from-green-700 hover:to-green-800 text-white cursor-pointer">
             <IoIosArrowForward />
           </button>
         </div>
