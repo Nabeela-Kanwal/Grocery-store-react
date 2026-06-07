0bb888e5720e183e549929c73612b46bf61d9c7a
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-07 21:16:19 +0500

    changes

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -18,8 +18,12 @@ const Testimonials = () => {
         </div>
 
         <div className="bg-zinc-100 rounded-xl p-8">
-          <div>
+          <div className="flex gap-5 items-center">
             <div className="w-16 h-16 rounded-full bg-red-500 outline-2 outline-green-700 outline-offset-4"></div>
+            <div>
+              <h5 className="text-xl font-bold">Name</h5>
+              <p className="text-zinc-600">Profession</p>
+            </div>
           </div>
         </div>
       </div>
