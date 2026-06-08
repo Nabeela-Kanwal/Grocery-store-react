597f4cfbf777f468eb723f8921ed1f84837f7a39
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-08 22:05:01 +0500

    remove

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -8,8 +8,7 @@ import Customer2 from "../../assets/customer2.jpg";
 import Customer3 from "../../assets/customer3.jpg";
 import Customer4 from "../../assets/customer4.jpg";
 import Customer5 from "../../assets/customer5.jpg";
-import 'swiper/css';
-import 'swiper/css/navigation';
+
 
 const Testimonials = () => {
   return (
