f95a839846e19c03a456db7d77ac3451426378b9
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-11 14:18:52 +0500

    fixed all product page

--- src/components/AllProducts/AllProducts.jsx
+++ src/components/AllProducts/AllProducts.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Banner from "../Banner/Banner";
+import Cards from "../Cards/Cards";
+import Heading from "../Heading/Heading";
+import ProductList from "../ProductList/ProductList";
 
 const AllProducts = () => {
-  return <div>All Products</div>;
+  const renderCards = ProductList.map((product) => {
+    return <Cards key={product.id} product={product} />;
+  });
+
+  return (
+    <section>
+      <Banner />
+      <div className="max-w-[1400px] mx-auto px-10 py-20">
+        <Heading highlight="All" heading="Products" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-12">
+          {renderCards}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AllProducts;
