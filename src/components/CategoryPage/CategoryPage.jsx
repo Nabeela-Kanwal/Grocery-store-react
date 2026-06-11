cbcb47019fd7303fc72fadec1c991c478a25a42b
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-11 13:52:01 +0500

    changes

--- src/components/CategoryPage/CategoryPage.jsx
+++ src/components/CategoryPage/CategoryPage.jsx
@@ -5,14 +5,12 @@ import ProductList from "../ProductList/ProductList";
 
 const CategoryPage = () => {
   const renderProduct = ProductList.map((product) => {
-    return (
-      <Cards image={product.image} name={product.name} price={product.price} />
-    );
+    return <Cards key={product.id} product={product} />;
   });
   return (
     <div>
       <Banner />
-     <div className="grid grid-cols-1 md:grid-cols-4 gap-9 py-20 max-w-[1400px] max-auto pa-10">
+     <div className="grid grid-cols-1 md:grid-cols-4 gap-9 py-20 max-w-[1400px] mx-auto px-10">
          {renderProduct}
      </div>
     </div>
