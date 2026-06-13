015b481f1a9ee0a85c978a9cd6f63d2dc013586e
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-13 20:42:09 +0500

    banner bg

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,12 +7,13 @@ const Banner = ({ title = "Products", bgImage }) => {
 
   return (
     <div
-      className="bg-zinc-400 h-[50vh] mt-25 flex justify-center items-center"
+      className="h-[50vh] mt-25 flex justify-center items-center bg-center bg-cover relative"
       style={bannerStyle}
     >
-      <h2 className="text-5xl text-zinc-800 bg-white p-5 rounded-xl font-bold">
+      <h2 className="text-5xl text-zinc-800 bg-white p-5 rounded-xl font-bold z-10">
         {title}
       </h2>
+      <div className="bg-black/20  absolute inset-0"></div>
     </div>
   );
 };
