3e6493c6170a2137b3ca9e5e113e92c4450e2e12
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-06-13 20:37:09 +0500

    dairy banner

--- src/components/Dairy/Dairy.jsx
+++ src/components/Dairy/Dairy.jsx
@@ -1,7 +1,8 @@
 import CategoryPage from "../CategoryPage/CategoryPage";
+import bgDairy from "../../assets/dairy-banner.jpg";
 
 const Dairy = () => {
-  return <CategoryPage categories="Dairy" />;
+  return <CategoryPage categories="Dairy" bgImage={bgDairy} />;
 };
 
 export default Dairy;
